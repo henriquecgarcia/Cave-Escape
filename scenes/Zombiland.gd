a87290bbeec1920563c406c2c255d1dc82ff3af2
Henrique <34416111+henriquecgarcia@users.noreply.github.com>
2022-10-29 12:05:18 -0300

    [/] UwU

--- scenes/Zombiland.gd
+++ scenes/Zombiland.gd
@@ -3,20 +3,24 @@ extends Node2D
 onready var Zombies = $Zombies
 onready var timer = $Zombie_Spawn_Timer
 
-var curZombies = 0
 var spawnedZombies = 0
 var zCode = preload("res://codes/Zombie.tscn")
 
 func createZombies(amount = 1):
 	for i in amount:
+		#var pPos = Vector2(randf(), randf())
+		var pPos = Vector2(20, 20)
 		var b = zCode.instance()
-		b.position = Vector2(10, 10)
+		b.position = pPos
 		Zombies.add_child(b)
-		curZombies += 1
+		spawnedZombies += 1
 
 func _ready():
 	randomize()
 	timer.start(3)
+
+func GetAliveZombies():
+	return Zombies.get_child_count()
 
 func _on_Zombie_Spawn_Timer_timeout():
 	createZombies(1)
