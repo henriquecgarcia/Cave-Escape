80ff01a7f66cd54a3be987f61220f4bc17433e03
Henrique <34416111+henriquecgarcia@users.noreply.github.com>
2022-10-29 12:37:15 -0300

    [+] Zumbis spawnam em áreas random!!!

--- scenes/Zombiland.gd
+++ scenes/Zombiland.gd
@@ -2,14 +2,18 @@ extends Node2D
 
 onready var Zombies = $Zombies
 onready var timer = $Zombie_Spawn_Timer
+onready var rng = RandomNumberGenerator.new()
+onready var Player = $Player
 
 var spawnedZombies = 0
 var zCode = preload("res://codes/Zombie.tscn")
 
 func createZombies(amount = 1):
 	for i in amount:
-		#var pPos = Vector2(randf(), randf())
-		var pPos = Vector2(20, 20)
+		var r = rng.randi_range(0, $Spawns.get_child_count())
+		var pPos = $Spawns.get_child(r).position
+		#print(r, pPos)
+		#var pPos = Vector2(20, 20)
 		var b = zCode.instance()
 		b.position = pPos
 		Zombies.add_child(b)
@@ -23,5 +27,7 @@ func GetAliveZombies():
 	return Zombies.get_child_count()
 
 func _on_Zombie_Spawn_Timer_timeout():
+	if not Player.IsAlive():
+		return
 	createZombies(1)
 	timer.start(2)
